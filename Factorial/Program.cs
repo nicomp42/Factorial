84ee9b9c9b59f2cf8bfdb2cb8fea23b0c02bcc4c
Derick-Bellofatto <Bellofdk@mail.uc.edu>
2024-10-24 12:01:57 -0400

    Added documentation, added User input, Created Factorial module

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+*Name: Derick Bellofatto
+*email:  Bellofdk@mail.uc.edu
+*Assignment Number: Assignment inClassFactorial
+*Due Date: 10/24/2024
+*Course #/Section:   IS 3050 - 001
+* Semester / Year:   Fall 2024
+*Brief Description of the assignment:  Create Factorial module based on psudo code change request
+
+*Brief Description of what this module does: Prompts for a user's number, then calculates the number's Factorial
+*Citations: Original ~ https://github.com/nicomp42/Factorial
+*Anything else that's relevant:
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +23,19 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ComputeFactorial(10));
+            //Prompts for User input
+            Console.WriteLine("Input a number for factorial: ");
+            //User input parsed into an integer
+            int FactorialInput = int.Parse(Console.ReadLine());
+            //Writing the computed value
+            Console.WriteLine(ComputeFactorial(FactorialInput));
 
         }
-
+        /// <summary>
+        /// Calculates the factorial of a variable declared in the main module
+        /// </summary>
+        /// <param name="num">Number which the factorial should be calculated from</param>
+        /// <returns></returns>
         static int ComputeFactorial(int num)
         {
             int Factorial = 1;
